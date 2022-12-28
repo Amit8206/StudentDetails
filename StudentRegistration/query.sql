bea8080057bcd54a8f0c287e4bc79cded7da19fe
Amit8206 <amitkumarsahoo572@gmail.com>
2022-12-28 16:41:56 +0530

    second commit

---
+++ StudentRegistration/query.sql
@@ -0,0 +1,8 @@
+
+create Table
+
+query = "CREATE TABLE student (id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY, sfirstname varchar(30), slastname varchar(30),
+date varchar(50),email varchar(50),ffirstname varchar(50),flastname varchar(50),mfirstname varchar(50),mlastname varchar(50),
+gender varchar(50),nation varchar(50),address varchar(50), city varchar(50),country varchar(50),phone varchar(50),mobile varchar(50),
+hscname varchar(50),hscboard varchar(50),hscscore varchar(50),sscname varchar(50),sscboard varchar(50),sscscore varchar(50),
+passport varchar(50),hscmark varchar(50),sscmark varchar(50),semistermark varchar(50))";
--- StudentRegistration/util/database.js
+++ StudentRegistration/util/database.js
@@ -79,17 +79,7 @@ module.exports.add_student = function( studentnamefirst,  studentnamelast, date,
 
 
 
-// query = "CREATE TABLE students (id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY, sfirstname varchar(30), slastname varchar(30),
-// date varchar(50),email varchar(50),ffirstname varchar(50),flastname varchar(50),mfirstname varchar(50),mlastname varchar(50),
-// gender varchar(50),nation varchar(50),address varchar(50), city varchar(50),country varchar(50),phone varchar(50),mobile varchar(50),
-// hscname varchar(50),hscboard varchar(50),hscscore varchar(50),sscname varchar(50),sscboard varchar(50),sscscore varchar(50),
-// passport MEDIUMBLOB,hscmark MEDIUMBLOB,sscmark MEDIUMBLOB,semistermark MEDIUMBLOB)";
 
-// query = "CREATE TABLE student (id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY, sfirstname varchar(30), slastname varchar(30),
-// date varchar(50),email varchar(50),ffirstname varchar(50),flastname varchar(50),mfirstname varchar(50),mlastname varchar(50),
-// gender varchar(50),nation varchar(50),address varchar(50), city varchar(50),country varchar(50),phone varchar(50),mobile varchar(50),
-// hscname varchar(50),hscboard varchar(50),hscscore varchar(50),sscname varchar(50),sscboard varchar(50),sscscore varchar(50),
-// passport varchar(50),hscmark varchar(50),sscmark varchar(50),semistermark varchar(50))";
 
 
 
